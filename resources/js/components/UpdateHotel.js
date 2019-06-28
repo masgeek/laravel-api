fa547f5ba494249262ea057056523ce17ce12b6f
Sammy Barasa <barsamms@gmail.com>
2019-06-28 16:16:15 +0300

    triall crud for component

--- resources/js/components/UpdateHotel.js
+++ resources/js/components/UpdateHotel.js
@@ -18,6 +18,17 @@ class UpdateHotel extends Component {
             errors: []
         };
     }
+
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        console.log('Hello world'+id)
+    }
+
+    render() {
+        return (
+            <h1>We are here</h1>
+        )
+    }
 }
 
 export default UpdateHotel
